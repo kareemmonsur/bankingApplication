5a8e0e7dd9fbd3496141a52613a3f37256986cf8
Monsur Kareem <monsurkareem@gmail.com>
2021-10-12 16:22:18 +0100

    new add

---
+++ src/test/java/africa/semicolon/bankingApplication/data/repositories/customerRepositoryImplTest.java
@@ -0,0 +1,48 @@
+package africa.semicolon.bankingApplication.data.repositories;
+
+import africa.semicolon.bankingApplication.data.models.Bvn;
+import africa.semicolon.bankingApplication.data.models.Customer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class customerRepositoryImplTest {
+    private CustomerRepository customerRepository;
+    @BeforeEach
+    void setUp() {
+       customerRepository=new CustomerRepositoryImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+        Customer customer = new Customer();
+        Bvn bvn = new Bvn("23355", customer);
+        customer.setBvn(bvn.getId());
+        customer.setFirstName("ojo");
+        customer.setLastName("imma");
+        Customer savedCustomer = customerRepository.save(customer);
+        assertEquals(customer, savedCustomer);
+    }
+
+    @Test
+    void findByCustomerId() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void testDelete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
