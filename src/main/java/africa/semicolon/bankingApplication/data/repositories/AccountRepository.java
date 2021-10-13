23b8df0339360fcde837d1d6f581fca7df1c2ea3
Monsur Kareem <monsurkareem@gmail.com>
2021-10-13 06:16:57 +0100

    new add

---
+++ src/main/java/africa/semicolon/bankingApplication/data/repositories/AccountRepository.java
@@ -0,0 +1,10 @@
+package africa.semicolon.bankingApplication.data.repositories;
+
+import africa.semicolon.bankingApplication.data.models.Account;
+
+public interface AccountRepository {
+    Account save(Account account);
+    Account findAccountById(String id);
+    void delete(Account account);
+    void delete(String id);
+}
