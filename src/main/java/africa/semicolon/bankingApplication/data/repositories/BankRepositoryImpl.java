05b94cd362827db33662833d1e45fbd8e0169170
Monsur Kareem <monsurkareem@gmail.com>
2021-10-13 11:31:18 +0100

    new add

--- src/main/java/africa/semicolon/bankingApplication/data/repositories/BankRepositoryImpl.java
+++ src/main/java/africa/semicolon/bankingApplication/data/repositories/BankRepositoryImpl.java
@@ -1,13 +1,15 @@
 package africa.semicolon.bankingApplication.data.repositories;
 
-import africa.semicolon.bankingApplication.data.models.Account;
+//import africa.semicolon.bankingApplication.data.models.Account;
+//import africa.semicolon.bankingApplication.data.models.Bank;
+
 import africa.semicolon.bankingApplication.data.models.Bank;
 
 import java.util.ArrayList;
 import java.util.List;
 
     public class BankRepositoryImpl implements BankRepository {
-        private List<Account> banks = new ArrayList<>();
+        private List<Bank> banks = new ArrayList<>();
 
         @Override
         public Bank save(Bank bank) {
@@ -16,9 +18,9 @@ import java.util.List;
         }
 
         @Override
-        public Account findAccountById(String id) {
-            for (Account account : banks) {
-                if (bank.getCustomerId().equalsIgnoreCase(id)) return bank;
+        public Bank findBankById(String id) {
+            for (Bank bank : banks) {
+                if (bank.getBankId().equalsIgnoreCase(id)) return bank;
             }
             return null;
         }
--- src/test/java/africa/semicolon/bankingApplication/data/repositories/BankRepositoryImplTest.java
+++ src/test/java/africa/semicolon/bankingApplication/data/repositories/BankRepositoryImplTest.java
@@ -1,5 +1,92 @@
 package africa.semicolon.bankingApplication.data.repositories;
 
+import africa.semicolon.bankingApplication.data.models.Bank;
+import africa.semicolon.bankingApplication.data.models.Bvn;
+import africa.semicolon.bankingApplication.data.models.Customer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BankRepositoryImplTest {
+    private BankRepository bankRepository;
+    @BeforeEach
+    void setUp() {
+        bankRepository = new BankRepositoryImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+        Bank bank = new Bank("1");
+        bank.setName("gtb");
+        Bank savedBank = bankRepository.save(bank);
+        assertEquals(bank, savedBank);
+    }
+
+//    @Test
+//    void findByCustomerId() {
+//        var customer = new Customer();
+//        var bvn = new Bvn("23355", customer);
+//        customer.setBvn(bvn.getId());
+//        customer.setFirstName("ojo");
+//        customer.setLastName("imma");
+//        Customer savedCustomer = customerRepository.save(customer);
+//        var foundCustomer = customerRepository.findByCustomerId("23355");
+//        assertEquals(savedCustomer, foundCustomer);
+//
+//    }
+//
+//    @Test
+//    void delete() {
+//        Customer customer = new Customer();
+//        Bvn bvn = new Bvn("23355", customer);
+//        customer.setBvn(bvn.getId());
+//        customer.setFirstName("ojo");
+//        customer.setLastName("imma");
+//        customerRepository.save(customer);
+//
+//        assertNotNull(customerRepository.findByCustomerId("23355"));
+//
+//        customerRepository.delete("23355");
+//
+//        assertNull(customerRepository.findByCustomerId("23355"));
+//    }
+//
+//    @Test
+//    void testDelete() {
+//        Customer customer = new Customer();
+//        Bvn bvn = new Bvn("23355", customer);
+//        customer.setBvn(bvn.getId());
+//        customer.setFirstName("ojo");
+//        customer.setLastName("imma");
+//        customerRepository.save(customer);
+//
+//        assertNotNull(customerRepository.findByCustomerId("23355"));
+//
+//        customerRepository.delete(customer);
+//
+//        assertNull(customerRepository.findByCustomerId("23355"));
+//    }
+//
+//    @Test
+//    void findAll() {
+//        Customer customer = new Customer();
+//        Bvn bvn = new Bvn("23355", customer);
+//        customer.setBvn(bvn.getId());
+//        customer.setFirstName("ojo");
+//        customer.setLastName("imma");
+//        customerRepository.save(customer);
+//        customerRepository.save(customer);
+//        customerRepository.save(customer);
+//
+//        assertEquals(3, customerRepository.findAll().size());
+//
+//    }
 
 }
