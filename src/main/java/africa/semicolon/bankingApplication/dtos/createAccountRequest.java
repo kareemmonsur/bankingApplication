20a2bab7f9b0b54551d3ee2306d80ad7012bd48f
Monsur Kareem <monsurkareem@gmail.com>
2021-10-14 09:26:25 +0100

    new add

--- src/main/java/africa/semicolon/bankingApplication/dtos/createAccountRequest.java
+++ src/main/java/africa/semicolon/bankingApplication/dtos/createAccountRequest.java
@@ -2,10 +2,12 @@ package africa.semicolon.bankingApplication.dtos;
 
 import lombok.Data;
 
+@Data
 public class createAccountRequest {
-    @Data
     private String bankId;
     private String firstName;
     private String lastName;
     private String bvn;
+    private int numberOfAccount;
+
 }
